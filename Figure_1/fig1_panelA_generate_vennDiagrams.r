d1d2ab95b90785dfcc8ed83c9971023e31e38bcc
jaflynn5 <jaflynn@wustl.edu>
2021-03-24 12:51:42 -0500

    Update fig1_panelA_generate_vennDiagrams.r

--- Figure_1/fig1_panelA_generate_vennDiagrams.r
+++ Figure_1/fig1_panelA_generate_vennDiagrams.r
@@ -11,7 +11,7 @@ library(VennDiagram)
 # HyperDMRs
 eac_hyperDMRs = read.table("EAC_hyperDMRs.bed")
 gbm_hyperDMRs = read.table("GBM_hyperDMRs.bed")
-shared_hyperDMRs = read.table("shared_EAC_GBM_hyperDMRs_one_line_one_DMR_sorted.bed")
+shared_hyperDMRs = read.table("shared_EAC_GBM_hyperDMRs.bed")
 num_eac_hyperDMRs=dim(eac_hyperDMRs)[1]
 num_gbm_hyperDMRs=dim(gbm_hyperDMRs)[1]
 num_shared_hyperDMRs=dim(shared_hyperDMRs)[1]
@@ -21,7 +21,7 @@ draw.pairwise.venn(area1 = num_eac_hyperDMRs, area2 = num_gbm_hyperDMRs, cross.a
 # HypoDMRs
 eac_hypoDMRs = read.table("EAC_hypoDMRs.bed")
 gbm_hypoDMRs = read.table("GBM_hypoDMRs.bed")
-shared_hypoDMRs = read.table("shared_EAC_GBM_hypoDMRs_one_line_one_DMR_sorted.bed")
+shared_hypoDMRs = read.table("shared_EAC_GBM_hypoDMRs.bed")
 num_eac_hypoDMRs=dim(eac_hypoDMRs)[1]
 num_gbm_hypoDMRs=dim(gbm_hypoDMRs)[1]
 num_shared_hypoDMRs=dim(shared_hypoDMRs)[1]
